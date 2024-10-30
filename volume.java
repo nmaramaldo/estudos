5b65d0f5acb91569c942095c652817b5c86c4164
nmaramalado <nataliamramaldof@gmail.com>
2024-10-29 21:29:58 -0300

    adição de novo exercício

---
+++ volume.java
@@ -0,0 +1,14 @@
+public class volume {
+    public static void main(String[] args) {
+        double V, R, A;
+
+        R = 2.394;
+        A = 4.004;
+        V = 3.14159 * R * R * A;
+        
+        System.out.println("O volume da lata de óleo é " + V + " m³");
+
+        }
+
+    
+}
